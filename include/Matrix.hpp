8d4f4cc2aed55ae24f840430a9336b873249574c
Elviss Strazdins <elviss@elviss.lv>
2021-08-12 23:29:12 +0300

    Add dimensions to Matrix

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -1,7 +1,29 @@
+#include <memory>
+
 namespace ov
 {
     class Matrix final
     {
     public:
+        Matrix() = default;
+
+    private:
+        enum class Format
+        {
+            float32,
+            float64,
+            int8,
+            int16,
+            int32,
+            uint8,
+            uint16,
+            uint32
+        };
+
+        std::uint32_t format = 0;
+        std::size_t width = 0;
+        std::size_t height = 0;
+        std::size_t channels = 0;
+        std::unique_ptr<void*> data;
     };
 }

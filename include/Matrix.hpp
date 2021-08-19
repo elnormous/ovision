808da2f8d72f0ba11f3ba77607a548a17a826085
Elviss Strazdins <elviss@elviss.lv>
2021-08-19 06:00:54 +0300

    Implement copy assignment operator

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -7,19 +7,19 @@ namespace ov
     {
     public:
         Matrix() noexcept = default;
-        
+
         Matrix(std::size_t w, std::size_t h, std::size_t c = 1):
             width{w},
             height{h},
             channels{c},
             data{new T[w * h * c]{}}
         {}
-        
+
         ~Matrix()
         {
             delete [] data;
         }
-        
+
         Matrix(const Matrix& other) noexcept:
             width{other.width},
             height{other.height},
@@ -30,7 +30,7 @@ namespace ov
                       other.data + other.width * other.height * other.channels,
                       data);
         }
-        
+
         Matrix(Matrix&& other) noexcept:
             width{other.width},
             height{other.height},
@@ -42,21 +42,36 @@ namespace ov
             other.channels = 0;
             other.data = nullptr;
         }
-        
+
+        Matrix& operator=(const Matrix& other) noexcept
+        {
+            delete [] data;
+
+            width = other.width;
+            height = other.height;
+            channels = other.channels;
+            data = new T[other.width * other.height * other.channels];
+            std::copy(other.data,
+                      other.data + other.width * other.height * other.channels,
+                      data);
+
+            return *this;
+        }
+
         Matrix& operator=(Matrix&& other) noexcept
         {
             delete [] data;
-            
+
             width = other.width;
             height = other.height;
             channels = other.channels;
             data = other.data;
-            
+
             other.width = 0;
             other.height = 0;
             other.channels = 0;
             other.data = nullptr;
-            
+
             return *this;
         }
 
@@ -64,17 +79,17 @@ namespace ov
         {
             return data;
         }
-        
+
         auto end() const noexcept
         {
             return data + width * height * channels;
         }
-        
+
         auto begin() noexcept
         {
             return data;
         }
-        
+
         auto end() noexcept
         {
             return data + width * height * channels;
@@ -83,7 +98,7 @@ namespace ov
         auto getWidth() const noexcept { return width; }
         auto getHeight() const noexcept { return height; }
         auto getChannels() const noexcept { return channels; }
-        
+
     private:
         std::size_t width = 0;
         std::size_t height = 0;
--- test/tests.cpp
+++ test/tests.cpp
@@ -15,11 +15,11 @@ TEST_CASE("Matrix move construction")
 {
     ov::Matrix<int> matrix{1, 2, 3};
     ov::Matrix<int> other = std::move(matrix);
-    
+
     REQUIRE(matrix.getWidth() == 0);
     REQUIRE(matrix.getHeight() == 0);
     REQUIRE(matrix.getChannels() == 0);
-    
+
     REQUIRE(other.getWidth() == 1);
     REQUIRE(other.getHeight() == 2);
     REQUIRE(other.getChannels() == 3);
@@ -30,11 +30,11 @@ TEST_CASE("Matrix move assignment")
     ov::Matrix<int> matrix{1, 2, 3};
     ov::Matrix<int> other;
     other = std::move(matrix);
-    
+
     REQUIRE(matrix.getWidth() == 0);
     REQUIRE(matrix.getHeight() == 0);
     REQUIRE(matrix.getChannels() == 0);
-    
+
     REQUIRE(other.getWidth() == 1);
     REQUIRE(other.getHeight() == 2);
     REQUIRE(other.getChannels() == 3);
@@ -43,33 +43,33 @@ TEST_CASE("Matrix move assignment")
 TEST_CASE("Matrix const range loop")
 {
     const ov::Matrix<int> matrix{1, 2, 3};
-    
+
     std::size_t count = 0;
-    
+
     for (int v : matrix)
     {
         REQUIRE(v == 0);
         ++count;
     }
-    
+
     REQUIRE(count == 6);
 }
 
 TEST_CASE("Matrix range loop")
 {
     const ov::Matrix<int> matrix{1, 2, 3};
-    
+
     std::size_t count = 0;
-    
+
     for (int& v : matrix)
     {
         v = static_cast<int>(count);
         ++count;
     }
-    
+
     REQUIRE(count == 6);
     count = 0;
-    
+
     for (int v : matrix)
     {
         REQUIRE(v == static_cast<int>(count));
@@ -82,19 +82,46 @@ TEST_CASE("Matrix range loop")
 TEST_CASE("Matrix copy constructor")
 {
     const ov::Matrix<int> matrix{1, 2, 3};
-    
+
     std::size_t count = 0;
-    
+
     for (int& v : matrix)
     {
         v = static_cast<int>(count);
         ++count;
     }
-    
+
     REQUIRE(count == 6);
     count = 0;
-    
+
     const ov::Matrix<int> matrix2 = matrix;
+
+    for (int v : matrix2)
+    {
+        REQUIRE(v == static_cast<int>(count));
+        ++count;
+    }
+
+    REQUIRE(count == 6);
+}
+
+TEST_CASE("Matrix assignment")
+{
+    const ov::Matrix<int> matrix{1, 2, 3};
+
+    std::size_t count = 0;
+
+    for (int& v : matrix)
+    {
+        v = static_cast<int>(count);
+        ++count;
+    }
+
+    REQUIRE(count == 6);
+    count = 0;
+
+    ov::Matrix<int> matrix2;
+    matrix2 = matrix;
 
     for (int v : matrix2)
     {
